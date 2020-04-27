849c9f67a66575d0bb17ad0d0214b5ddc98c3488
Jamie Kielland <jamie@freshfx.no>
2020-04-27 13:30:03 +0200

    upd1

---
+++ script.js
@@ -0,0 +1,15 @@
+let attackBtn = document.getElementById("attackButton");
+let battleInfo = document.getElementById("battleInfo");
+let heroAttack = 45;
+let BossAttack = 73;
+
+function attackFunc()   {
+    let attackOne = Math.random(()  * 1.3);
+    let attackTwo = Math.random(()  * 1.9);
+    let attackCrit = Math.random(() * 3);
+
+    
+
+}
+
+
