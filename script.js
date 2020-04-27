d56df615e4866c914718a6d6d33d31699c7ce45a
Jamie Kielland <jamie@freshfx.no>
2020-04-27 15:08:13 +0200

    update 3

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ let heroAttack = 45;
 let BossAttack = 73;
 
 function attackFunc()   {
-    let attackOne = Math.random(()  * 1.3);
-    let attackTwo = Math.random(()  * 1.9);
-    let attackCrit = Math.random(() * 3);
+    let attackOne = Math.random((heroAttack)  * 1.3);
+    let attackTwo = Math.random((heroAttack)  * 1.9);
+    let attackCrit = Math.random((heroAttack) * 3);
+    let attackRandom = [attackOne, attackTwo, attackCrit];
+    
+    const randomAttack = attackRandom[Math.floor(Math.random() * attackRandom.length)];
+
+    
 
 
 }
